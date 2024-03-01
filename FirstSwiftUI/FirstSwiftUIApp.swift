4a3e753f6af1523c7defdd7e299e36bc8a50f0a3
urvi-k <urvi151212@gmail.com>
2024-03-01 00:18:11 -0500

    Initial Commit

---
+++ FirstSwiftUI/FirstSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  FirstSwiftUIApp.swift
+//  FirstSwiftUI
+//
+//  Created by Urvi Koladiya on 2024-03-01.
+//
+
+import SwiftUI
+
+@main
+struct FirstSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
