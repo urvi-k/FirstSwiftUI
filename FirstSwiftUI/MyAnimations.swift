91c225136f7e539f321697f56c80250bb206d082
urvi-k <urvi151212@gmail.com>
2024-05-04 09:31:05 -0400

    feat: update animation

--- FirstSwiftUI/MyAnimations.swift
+++ FirstSwiftUI/MyAnimations.swift
@@ -10,27 +10,34 @@ import SwiftUI
 struct MyAnimations: View {
     
     @State var isAnimated: Bool = false
+    @State var isAnimated2: Bool = false
     
     var body: some View {
         VStack {
             
             
             Button("Go") {
-                withAnimation(Animation.default.repeatForever(autoreverses: true)) {
+               // withAnimation(Animation.default.repeatForever(autoreverses: true)) {
                     isAnimated.toggle()
-                }
+                isAnimated2.toggle()
+               // }
             }
             Spacer()
-            RoundedRectangle(cornerRadius: isAnimated ? 100 : 50)
+            RoundedRectangle(cornerRadius: isAnimated ? 50 : 25)
                 .fill(isAnimated ? .mint : .indigo)
-                .rotationEffect(.degrees(isAnimated ? 360 : 0))
                 .frame(
-                    width: isAnimated ? 100 : 300,
-                    height: isAnimated ? 100 : 300)
-                .offset(y: isAnimated ? 300 : 0)
-                
-               // .animation(Animation.default.repeatForever(autoreverses: true), value: 0.3) // not working
+                    width: isAnimated ? 100 : 200,
+                    height: isAnimated ? 100 : 200)
+                .rotationEffect(.degrees(isAnimated ? 360 : 0))
+                //.offset(y: isAnimated ? 300 : 0)
+                .animation(Animation.default.repeatForever(autoreverses: true), value: isAnimated)
             Spacer()
+            
+            RoundedRectangle(cornerRadius: 20)
+                .fill(.green)
+                .frame(width: isAnimated2 ? 400 : 200, height: 100)
+                .animation(.spring, value: isAnimated2)
+            
         }
     }
 }
